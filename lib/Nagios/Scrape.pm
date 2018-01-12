8613e10dee04f36d7521010ff3fb275f8d6e6a40
David Precious <davidp@preshweb.co.uk>
2018-01-12 09:39:59 +0000

    Reflect transfer of maintainer to me

--- lib/Nagios/Scrape.pm
+++ lib/Nagios/Scrape.pm
@@ -282,14 +282,16 @@ sub decode_html {
 
 =head1 AUTHOR
 
-Joe Topjian, C<< <joe at terrarum.net> >>
+Original author: Joe Topjian, C<< <joe at terrarum.net> >>
+
+Maintainership since 2018: David Precious (BIGPRESH) C<< davidp@preshweb.co.uk >>
+
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-nagios-scrape at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Nagios-Scrape>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests on GitHub:
 
+L<https://github.com/bigpresh/Nagios-Scrape/issues>
 
 
 
